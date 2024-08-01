bf9ec292af52e3aa6575a88722497b2ddba72e22
zanebeidas <bzane591@gamil.com>
2024-08-01 15:44:49 -0400

    Fix again maybe?

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ size = (500, 400)
 
 # Create the screen and change the color with the Fill function
 screen = pyg.display.set_mode(size)
-screen.fill(pyg.Color(255, 255, 0, 50))
+screen.fill(pyg.Color(255, 255, 0))
 
 # Start a loop that will run forever since we don't want to game to stop unless it is forced to
 while True:
@@ -19,7 +19,7 @@ while True:
             mouse_position = pyg.mouse.get_pos()
             circle_thickness = 5
             border_thickness = 7
-            screen.fill(pyg.Color(255, 255, 0, 50))
+            screen.fill(pyg.Color(255, 255, 0))
 
             # Draw the circle
             pyg.draw.circle(screen, (0, 0, 0), mouse_position, circle_thickness, border_thickness)
