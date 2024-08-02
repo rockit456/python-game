22167cff34df151de09609bdf788e9b596464161
Eli Beidas <ebeidas@Elis-MacBook-Pro.local>
2024-08-01 20:32:44 -0400

    started of airlines simulator

---
+++ game.py
@@ -0,0 +1,6 @@
+airlines_name=input("Welcome to airlines simulator, to get started, you have to name your airlines. ")
+airnamemabye=input("Are yoy sure "+airlines_name+" is your airlines name?(y or n response)")
+if airnamemabye==yes:
+    planetype=input("Cool name, now you have to choose your first airplane. The Swift 280 with a compasity of 16 people (Costs 50 dollors per flite) or the Blitz 160 with a compasity of 24(Costs 75 dollors per flite )(1 or 2 responce)")
+elif airlines_name2==input("choose your airlines name"):
+    planetype=input("Cool name, now you have to choose your first airplane. The Swift 280 with a compasity of 16 people (Costs 50 dollors per flite) or the Blitz 160 with a compasity of 24(Costs 75 dollors per flite )(1 or 2 responce)")
